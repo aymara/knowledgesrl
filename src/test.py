7a4368bd4e3477300dd07408f622af4e55a106d2
quentin <quentin@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-04-29 09:47:26 +0000

    Test all modules in one fell swoop
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18173 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

---
+++ src/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from mst_tree_builder import *
+from framenetreader import *
+
+if __name__ == '__main__':
+    unittest.main()
