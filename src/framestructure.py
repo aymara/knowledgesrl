4ed8dac9a0645d6d52b79a6794d452aecff192bc
guilhem <guilhem@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-05-02 10:15:42 +0000

    Add a class for VerbNet structure representation and a converter from Frame to this class
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18199 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

--- src/framestructure.py
+++ src/framestructure.py
@@ -3,6 +3,8 @@
 
 """Frames, arguments and predicates."""
 
+import unittest
+
 class Frame:
     """A frame extracted from the corpus 
     
@@ -24,6 +26,118 @@ class Frame:
             self.predicate == other.predicate and
             self.args == other.args and
             self.words == other.words)
+
+class VerbnetFrame:
+    """A representation of a frame syntaxic structure
+    
+    :var structure: String containing a VerbNet-style representation of the structure
+    :var roles: List of the VerbNet of the element of the structure empty for prepositions
+    
+    """
+    
+    #TODO : complete this list
+    keywords = ["to be", "how", "to", "what", "whether"]
+    
+    def __init__(self, structure, roles):
+        self.structure = structure
+        self.roles = roles
+        
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and
+            self.structure == other.structure and
+            self.roles == other.roles)
+    
+    @staticmethod    
+    def build_from_frame(frame):
+        """Build a VerbNet frame from a Frame object
+        
+        :param frame: The original Frame.
+        :type frame: Frame.
+        :returns: VerbnetFrame -- the built frame, without the roles
+        """
+        
+        # The main job is to build the VerbNet structure representation
+        # from the Frame object data
+        
+        # First, delete everything that is before or after the frame
+        begin = frame.predicate.begin
+        end = frame.predicate.end
+        
+        for argument in frame.args:
+            if not argument.instanciated: continue
+            if argument.begin < begin: begin = argument.begin
+            if argument.end > end: end = argument.end
+ 
+        structure = frame.sentence[begin:end + 1]
+        # Then, replace the predicate/arguments by their phrase type
+        structure = VerbnetFrame._reduce_args(frame, structure, begin)
+        # And delete everything else, except some keywords
+        structure = VerbnetFrame._keep_only_keywords(structure)
+        
+        return VerbnetFrame(structure, [])
+    
+    @staticmethod    
+    def _reduce_args(frame, structure, new_begin):
+        """Replace the predicate and the argument of a frame by phrase type marks
+        
+        :param frame: The original Frame.
+        :type frame: Frame.
+        :param structure: The current structure representation.
+        :type structure: str.
+        :param new_begin: The left offset cause by previous manipulations.
+        :type new_begin: int.
+        :returns: String -- the reduced string
+        """
+        predicate_begin = frame.predicate.begin - new_begin
+        predicate_end = frame.predicate.end - new_begin
+
+        for argument in reversed(frame.args):
+            if not argument.instanciated: continue
+
+            structure = "{}< {}>{}".format(
+                structure[0:argument.begin - new_begin], 
+                argument.phrase_type, 
+                structure[1 + argument.end - new_begin:])
+                
+            if argument.begin - new_begin < predicate_begin:
+                offset = (argument.end - argument.begin + 1) - (3 + len(argument.phrase_type))
+                predicate_begin -= offset
+                predicate_end -= offset
+
+        structure = "{}< V>{}".format(
+            structure[0:predicate_begin], structure[1+predicate_end:])
+            
+        return structure
+    
+    @staticmethod        
+    def _keep_only_keywords(sentence):
+        """Keep only keywords and phrase type markers in the structure
+        
+        :param sentence: The structure to reduce.
+        :type sentence: str.
+        :returns: String -- the reduced string
+        """
+        pos = 0
+        last_pos = len(sentence) - 1
+        inside_tag = False
+        result = ""
+        
+        while pos < last_pos:
+            if sentence[pos] == ">": inside_tag = False
+            if inside_tag: result += sentence[pos]
+            if sentence[pos] == "<": inside_tag = True
+            
+            for search in VerbnetFrame.keywords:
+                if " "+search == sentence[pos:pos + len(search) + 1]:
+                    pos += len(search) + 1
+                    result += " "+search
+            
+            pos += 1
+        
+        if result[0] == " ": result = result[1:]
+        if result[-1] == " ": result = result[:-1]
+            
+        return result
         
 class Arg:
 
@@ -124,3 +238,42 @@ class Word:
     def __repr__(self):
         return "Word({}, {}, \"{}\")".format(self.begin, self.end, self.pos)
 
+            
+class VerbnetFrameTest(unittest.TestCase):
+    def test_conversion(self):
+        tested_frames = [
+            Frame(
+                "Rep . Tony Hall , D- Ohio , urges the United Nations to allow"+\
+                " a freer flow of food and medicine into Iraq .", 
+                Predicate(28, 32, "urges", "urge"),
+                [
+                    Arg(34, 51, "the United Nations", "Addressee", True, "NP"),
+                    Arg(53, 104,
+                        "to allow a freer flow of food and medicine into Iraq", 
+                        "Content", True, "VPto"),
+                    Arg(0, 26, "Rep . Tony Hall , D- Ohio", "Speaker", True, "NP")
+                ] ),
+            Frame(
+                "Rep . Tony Hall , D- Ohio , urges the United Nations to allow"+\
+                " a freer flow of food and medicine into Iraq .", 
+                 Predicate(56, 60, "allow", "allow"),
+                 [
+                    Arg(62, 104, 
+                        "a freer flow of food and medicine into Iraq",
+                        "Action", True, "NP"),
+                    Arg(34, 51, "the United Nations", "Grantee", True, "NP"),
+                    Arg(0, -1, "", "Grantor", False, "")
+                 ] ) ]
+        
+        expected_results = [
+            VerbnetFrame("NP V NP VPto", []),
+            VerbnetFrame("NP to V NP", [])
+        ]
+            
+        verbnet_frame = VerbnetFrame.build_from_frame(tested_frames[0])
+        self.assertEqual(expected_results[0], verbnet_frame)
+        verbnet_frame = VerbnetFrame.build_from_frame(tested_frames[1])
+        self.assertEqual(expected_results[1], verbnet_frame)
+        
+if __name__ == "__main__":
+    unittest.main()
