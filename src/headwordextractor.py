1233f375678416fd014c2644b538de4343b908f4
guilhem <guilhem@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-05-24 10:07:42 +0000

    Add a [-s n file] option that make the script send n headword extraction exemples into file
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18373 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

--- src/headwordextractor.py
+++ src/headwordextractor.py
@@ -11,6 +11,7 @@ from conllreader import SyntacticTreeBuilder, SyntacticTreeNode
 import framenetreader
 
 import pickle
+import getopt
 
 class HeadWordExtractor:
     def __init__(self, path):
@@ -56,7 +57,10 @@ class HeadWordExtractor:
         else: self.words.add(word)
         
         return word
-    
+        
+    def best_node(self, arg_text):
+        return self.tree.closest_match_as_node(arg_text)
+
     def get_class(self, word):
         if word in self.word_classes:
             return self.word_classes[word]
@@ -64,44 +68,20 @@ class HeadWordExtractor:
         
     def _get_headword(self, arg_text):
         node = self.tree.closest_match_as_node(arg_text)
-        return node.word
+        return node.word, node.pos
   
 class HeadWordExtractorTest(unittest.TestCase):
     def comp(self, original, parsed):
         return all(
             [x == y or y == "<num>" for x,y in zip(original.split(), parsed.split())]
         )
-    
-    def test_classes(self):
-        corpus_path = "../data/fndata-1.5/fulltext/"
-        filename = "ANC__110CYL067"
-        extractor = HeadWordExtractor("../data/framenet_parsed/")
-        extractor.load_file(filename+".conll")
-
-        reader = framenetreader.FulltextReader(corpus_path+filename+".xml", False)    
-
-        for frame in reader.frames:
-            extractor.select_sentence(frame.sentence_id)
-            for arg in frame.args:
-                extractor.headword(arg.text)
-
-        extractor.words.add("abcde")
-        extractor.compute_word_classes()
-        self.assertEqual(extractor.get_class("soda"), "physical_entity.n.01")
-        self.assertEqual(extractor.get_class("I"), "pronoun")
-        self.assertEqual(extractor.get_class("Goodwill"), "proper_noun")
-        self.assertEqual(extractor.get_class("abcde"), "unknown")
-        self.assertEqual(extractor.get_class("fghij"), "unknown")
-
-    def test_sentences_match(self):
-        corpus_path = "../data/fndata-1.5/fulltext/"
-        extractor = HeadWordExtractor("../data/framenet_parsed/")
         
-        bad_files = [
+    corpus_path = "../data/fndata-1.5/fulltext/"
+    bad_files = [
             "ANC__110CYL070.xml", "C-4__C-4Text.xml",
             "NTI__BWTutorial_chapter1.xml", "NTI__LibyaCountry1.xml",
             "NTI__NorthKorea_Introduction.xml"]
-        bad_sentences = [
+    bad_sentences = [
             ("LUCorpus-v0.3__sw2025-ms98-a-trans.ascii-1-NEW.xml", 9),
             ("NTI__Iran_Chemical.xml", 6),
             ("NTI__Iran_Chemical.xml", 62),
@@ -112,35 +92,64 @@ class HeadWordExtractorTest(unittest.TestCase):
             ("PropBank__ElectionVictory.xml", 5),
             ("PropBank__ElectionVictory.xml", 9),
             ("PropBank__LomaPrieta.xml", 18)]
+            
+    def test_classes(self):
+        filename = "ANC__110CYL067"
+        extractor = HeadWordExtractor("../data/framenet_parsed/")
+        extractor.load_file(filename+".conll")
 
-        for filename in sorted(os.listdir(corpus_path)):
+        reader = framenetreader.FulltextReader(self.corpus_path+filename+".xml", False)    
+
+        for frame in reader.frames:
+            extractor.select_sentence(frame.sentence_id)
+            for arg in frame.args:
+                extractor.headword(arg.text)
+
+        extractor.words.add("abcde")
+        extractor.compute_word_classes()
+        self.assertEqual(extractor.get_class("soda"), "physical_entity.n.01")
+        self.assertEqual(extractor.get_class("I"), "pronoun")
+        self.assertEqual(extractor.get_class("abcde"), "unknown")
+        self.assertEqual(extractor.get_class("fghij"), "unknown")
+
+    def test_sentences_match(self, num_sample = 0):
+        extractor = HeadWordExtractor("../data/framenet_parsed/")
+
+        sample = []
+        for filename in sorted(os.listdir(self.corpus_path)):
             if not filename[-4:] == ".xml": continue
 
-            if filename in bad_files: continue
+            if filename in self.bad_files: continue
             
             print(filename, file=sys.stderr)
             
             extractor.load_file(filename)
             
-            reader = framenetreader.FulltextReader(corpus_path+filename, False)
+            reader = framenetreader.FulltextReader(self.corpus_path+filename, False)
             previous_sentence = 0
 
             for frame in reader.frames:
-                if (filename, frame.sentence_id) in bad_sentences: continue
+                if (filename, frame.sentence_id) in self.bad_sentences: continue
    
                 if frame.sentence_id != previous_sentence:
                     extractor.select_sentence(frame.sentence_id)
-
+                
+                for arg in frame.args:
+                    node = extractor.best_node(arg.text)
+                    sample.append((arg.text, node.flat(), node.word))
+                
                 self.assertTrue(self.comp(frame.sentence, extractor.current_sentence()))
                 previous_sentence = frame.sentence_id
+                
+        random.shuffle(sample)
+        return sample[0:num_sample]
     
     def test_1(self):
-        corpus_path = "../data/fndata-1.5/fulltext/"
         filename = "ANC__110CYL067"
         extractor = HeadWordExtractor("../data/framenet_parsed/")
         extractor.load_file(filename+".conll")
 
-        reader = framenetreader.FulltextReader(corpus_path+filename+".xml", False)    
+        reader = framenetreader.FulltextReader(self.corpus_path+filename+".xml", False)    
 
         frame = reader.frames[1]
         extractor.select_sentence(frame.sentence_id)
@@ -150,4 +159,22 @@ class HeadWordExtractorTest(unittest.TestCase):
         self.assertTrue(extractor.headword(frame.args[0].text) == "contribution")
 
 if __name__ == "__main__":
-    unittest.main()
+    options = getopt.getopt(sys.argv[1:], "s:", [])
+    num_sample = 0
+    filename = ""
+
+    for opt, value in options[0]:
+        if opt == "-s": 
+            if len(options[1]) < 1:
+                print("Syntax : headwordextractor [-s num file]")
+                exit(0)
+            num_sample = int(value)
+            filename = options[1][0]
+        
+    if num_sample > 0:
+        tester = HeadWordExtractorTest()
+        result = tester.test_sentences_match(num_sample)
+        with open(filename, "wb") as picklefile:
+            pickle.dump(result, picklefile)
+    else:
+        unittest.main()
