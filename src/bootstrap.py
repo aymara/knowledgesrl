11e972cd0ceb1f12cbe1fba8a87df108ff860e9c
guilhem <guilhem@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-05-27 07:46:48 +0000

    Fix bootstrap module (the content of :slots was never updated)
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18396 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -12,13 +12,13 @@ def bootstrap_algorithm(frames, probability_model, hw_extractor, verbnet_classes
     log_ratio_step = 0.5
     min_evidence = [3, 5, 10]
     
-    
     # Transform the frame list (with the slots in frame.roles) into
     # a list of (frame, role_set, frame_position, slot_position)
     grouped_roles = [[(x, y, i, j) for j, y in enumerate(x.roles)] for i, x in enumerate(frames)]
     slots = reduce(lambda a,b:a+b, grouped_roles)
     
     while log_ratio > 0:
+        # Update probability model with resolved slots
         for frame, role_set, i, j in slots:
             if len(role_set) == 1:
                 probability_model.add_data_bootstrap(
@@ -34,17 +34,18 @@ def bootstrap_algorithm(frames, probability_model, hw_extractor, verbnet_classes
         # Remove resolved and empty slots
         slots = list(filter(lambda x: len(x[1]) > 1, slots))
  
+        # According to the article, there is no longer a min evidence threshold
+        # when log_ratio reaches 1
         if log_ratio == 1: min_evidence = [1, 1, 1]
         
         for frame, role_set, i, j in slots:
-            if len(role_set) <= 1: continue
-            
             role = None
             for backoff_level in [0, 1, 2]:
                 role1, role2, ratio = probability_model.best_roles_bootstrap(
                     role_set,
                     frame.predicate,
-                    verbnet_classes[frame.predicate][0], #Randomly choose the first class
+                    # Choosing the first class here is arbitrary
+                    verbnet_classes[frame.predicate][0],
                     frame.slot_types[j],
                     frame.slot_preps[j],
                     frame.headwords[j],
@@ -61,6 +62,12 @@ def bootstrap_algorithm(frames, probability_model, hw_extractor, verbnet_classes
                     break
 
             if role != None:
-                frames[i].roles[j] = set([role])
+                role_set.intersection_update(set())
+                role_set.add(role)
+                # role_set = set([role]) does not work
+                # because we need the role set to be updated
+                # everywhere it is referenced
+
         log_ratio -= log_ratio_step
+
 
