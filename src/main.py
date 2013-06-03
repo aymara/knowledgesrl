80bfb2fbadfa1d43abf024657acccd548a0a8c3c
guilhem <guilhem@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-06-03 08:12:43 +0000

    Commented code deletion
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18473 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

--- src/main.py
+++ src/main.py
@@ -38,36 +38,6 @@ def init_fn_reader(path):
     return reader
 
 if __name__ == "__main__":
-    """# Default values for command-line options
-    matching_algorithm = "sync_predicates"
-    core_args_only = True
-    gold_args = True
-    debug = False
-    bootstrap = False
-    probability_model = "predicate_slot"
-
-    options = getopt.getopt(sys.argv[1:], "d:",
-        ["fmatching-algo=", "add-non-core-args",
-        "model=", "bootstrap", "no-gold-args"])
-    for opt,value in options[0]:
-        if opt == "-d":
-            debug = True
-            value = 0 if value == "" else int(value)
-            if value > 0:
-                n_debug = value
-        if opt == "--fmatching-algo":
-            matching_algorithm = value
-        if opt == "--add-non-core-args":
-            core_args_only = False
-        if opt == "--model":
-            if not value in probabilitymodel.models:
-                raise Exception("Unknown model {}".format(value))
-            probability_model = value
-        if opt == "--bootstrap":
-            bootstrap = True
-        if opt == "--no-gold-args":
-            gold_args = False"""
- 
     verbnet_predicates, verbnet_classes = init_verbnet(paths.VERBNET_PATH)
 
     print("Loading frames...", file=sys.stderr)
