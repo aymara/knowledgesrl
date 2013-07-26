c87ee22cb96216793b13ec9a6d3516a5dbb71774
guilhem <guilhem@7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5>
2013-07-26 12:57:16 +0000

    Beginning of Semafor results evaluation
    git-svn-id: svn+ssh://tangerine/mnt/bay/subversion/worksvnroot/trunk/SemanticRoleLabeling/BaselineSRLRobert@18735 7fff26f0-e11d-0410-b8d0-f4b6ff9b0dc5

---
+++ src/main_semafor.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import framenetreader
+import os
+import roleextractor
+import verbnetreader
+import rolematcher
+import paths
+import options
+
+
+semafor_file = "../data/semafor/semafor_testset.out"
+semafor_corpus = "../data/semafor/testset/"
+
+reader = framenetreader.FulltextReader(
+semafor_file, core_args_only = True)
+semafor_frames = reader.frames
+semafor_roles = [[y.role for y in x.args] for x in semafor_frames]
+
+annotated_frames = []
+files = os.listdir(semafor_corpus)
+for filename in sorted(files):
+    if not filename[-4:] == ".xml": continue
+    reader = framenetreader.FulltextReader(
+        semafor_corpus + filename,
+        core_args_only = True,
+        keep_unannotated = True)
+    annotated_frames += reader.frames
+
+num_annotated_frames = len(annotated_frames)
+num_annotated_args = sum([len(x.args) for x in annotated_frames])
+
+num_good, num_bad = 0, 0
+num_correct, num_incorrect = 0, 0    
+
+sentence_position = 0
+last_sentence = ""
+for frame in semafor_frames:
+    if last_sentence != frame.sentence:
+        sentence_found = True
+        i = sentence_position
+        while annotated_frames[i].sentence != frame.sentence:
+            if i >= len(annotated_frames) - 1:
+                sentence_found = False
+                break
+            i += 1
+        if sentence_found:
+            sentence_position = i
+
+    last_sentence = frame.sentence    
+    if not sentence_found: continue
+
+    i = sentence_position
+    frame_found = True
+    while annotated_frames[i].predicate.begin != frame.predicate.begin:
+        if (i >= len(annotated_frames) - 1
+            or annotated_frames[i + 1].sentence != frame.sentence
+        ):
+            frame_found = False
+            break
+        i += 1
+    if not frame_found: continue
+    
+    for arg in frame.args:
+        arg_found = False
+        for annotated_arg in annotated_frames[i].args:
+            if annotated_arg.begin == arg.begin and annotated_arg.end == arg.end:
+                arg_found = True
+                num_good += 1
+                if arg.role == annotated_arg.role:
+                    num_correct += 1
+                else:
+                    num_incorrect += 1
+                break
+        if not arg_found:
+            num_bad += 1
+
+print(num_annotated_args)
+print("{} {} {} {}".format(num_good, num_bad, num_correct, num_incorrect))
