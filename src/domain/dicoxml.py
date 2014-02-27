091e1966f02ccd2fb8cfe24d70b97518d4f51514
Quentin Pradet <quentin.pradet@gmail.com>
2014-02-27 10:51:48 +0100

    Move dico xml functions to a specific module

---
+++ src/domain/dicoxml.py
@@ -0,0 +1,50 @@
+"""
+Helpers to manage the way dicoinfo and dicoenviro are represented as XML
+"""
+
+
+def deindent_text(sentence_text):
+    """
+    Deindent text found in the indent XML files.
+
+    'Long   sentence\n      cut' would become 'Long   sentence cut'
+    """
+
+    mode = 'keep'
+    last_real_char = ''
+    origin_sentence = ''
+
+    for c in sentence_text:
+        if c == '\n':
+            mode = 'throw away'
+        else:
+            if mode == 'keep':
+                origin_sentence += c
+                last_real_char = c
+            elif mode == 'throw away':
+                if c != ' ':
+                    mode = 'keep'
+                    if last_real_char != '':
+                        origin_sentence += ' '
+                    origin_sentence += c
+                    last_real_char = c
+
+    return origin_sentence
+
+
+def get_all_text(elem, get_tail=False):
+    if not elem:
+        return ''
+
+    all_text = elem.text if elem.text else ''
+
+    for c in elem:
+        all_text += get_all_text(c, get_tail=True)
+
+    if get_tail:
+        all_text += elem.tail if elem.tail else ''
+    else:
+        all_text = deindent_text(all_text)
+
+    return all_text
+
--- src/domain/extract_syntactic_data.py
+++ src/domain/extract_syntactic_data.py
@@ -11,51 +11,8 @@ from colorama import Fore
 import paths
 import verbnet
 from rolemapping import RoleMapping
+from dicoxml import deindent_text, get_all_text
 
-
-def deindent_text(sentence_text):
-    """
-    Deindent text found in the indent XML files.
-
-    'Long   sentence\n      cut' would become 'Long   sentence cut'
-    """
-
-    mode = 'keep'
-    last_real_char = ''
-    origin_sentence = ''
-
-    for c in sentence_text:
-        if c == '\n':
-            mode = 'throw away'
-        else:
-            if mode == 'keep':
-                origin_sentence += c
-                last_real_char = c
-            elif mode == 'throw away':
-                if c != ' ':
-                    mode = 'keep'
-                    if last_real_char != '':
-                        origin_sentence += ' '
-                    origin_sentence += c
-                    last_real_char = c
-
-    return origin_sentence
-
-def get_all_text(elem, get_tail=False):
-    if not elem:
-        return ''
-
-    all_text = elem.text if elem.text else ''
-
-    for c in elem:
-        all_text += get_all_text(c, get_tail=True)
-
-    if get_tail:
-        all_text += elem.tail if elem.tail else ''
-    else:
-        all_text = deindent_text(all_text)
-
-    return all_text
 
 def retrieve_constructs(dico, xmlns):
     frames_for_lexie = collections.defaultdict(list)
