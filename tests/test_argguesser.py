da4e75525b66403245a8c6f63afa80a055f889e7
Gaël de Chalendar <gael.de-chalendar@cea.fr>
2024-12-02 15:46:48 +0100

    Correct sending arguments to test class

--- tests/test_argguesser.py
+++ tests/test_argguesser.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
+import logging
 import os
+import sys
 
 import argparse
 import paths
@@ -14,91 +15,18 @@ from conllreader import SyntacticTreeBuilder
 from framenetframe import Arg
 import probabilitymodel
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+logging.root.setLevel(logging.DEBUG)
+
+logger.setLevel(Options.loglevel)
+
+
 class ArgGuesserTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # parse command line arguments
-        parser = argparse.ArgumentParser(
-            description="""
-            Depending on the options used, can
-            # Annotate a single file
-            knowledgesrl.py --conll_input=parsed_file.conll
-                    --conll_output=annotated_file.conll [options]
-
-            # Annotate FrameNet test set
-            knowledgesrl.py [options]
-
-            # Annotate FrameNet training set
-            knowledgesrl.py --training-set [options]
-
-            # Annotate FrameNet example corpus
-            knowledgesrl.py --lu [options]
-            """)
-        parser.add_argument("--language", "-l", type=str, choices=["eng", "fre"],
-                            default="eng",
-                            help="Name of the CoNLL-U file with the gold data.")
-        parser.add_argument("--best-gold", action="store_true",
-                            help=" Best configuration for gold.")
-        parser.add_argument("--best-auto", action="store_true",
-                            help="Best configuration for auto.")
-        parser.add_argument("--matching-algorithm", type=str,
-                            choices=["baseline", "sync_predicates",
-                                        "stop_on_fail"],
-                            default="sync_predicates",
-                            help="Select a frame matching algorithm.")
-        parser.add_argument("--add-non-core-args", action="store_true",
-                            help="Consider non-core-arg with gold arguments (why?)")
-        parser.add_argument("--model", type=str,
-                            choices=probabilitymodel.models,
-                            help="Probability models.")
-        parser.add_argument("--bootstrap", action="store_true",
-                            help="")
-        parser.add_argument("--no-argument-identification", action="store_true",
-                            help="Identify arguments automatically")
-        parser.add_argument("--heuristic-rules", action="store_true",
-                            help="Use Lang and Lapata heuristics to find args.")
-        parser.add_argument("--passivize", action="store_true",
-                            help="Handle passive sentences")
-        parser.add_argument("--semantic-restrictions", action="store_true",
-                            help="Restrict to phrases that obey VerbNet restrictions")
-        parser.add_argument("--wordnet-restrictions", action="store_true",
-                            help="Restrict to phrases that obey WordNet restrictions")
-        # what do we annotate?
-        parser.add_argument("--conll-input", "-i", type=str, default="",
-                            help="File to annotate.")
-        parser.add_argument("--conll-output", "-o", type=str, default=None,
-                            help="File to write result on. Default to stdout.")
-        parser.add_argument("--corpus", type=str,
-                            choices=["FrameNet", "dicoinfo_fr"],
-                            default=None,
-                            help="")
-        parser.add_argument("--training-set", action="store_true", default=True,
-                            help="To annotate FrameNet training set.")
-        parser.add_argument("--lu", action="store_true",
-                            help="To annotate FrameNet example corpus.")
-        # what kind of output do we want
-        parser.add_argument("--frame-lexicon", type=str,
-                            choices=["VerbNet", "FrameNet"],
-                            default="VerbNet",
-                            help="Chose frame lexicon to use for output.")
-        # meta
-        parser.add_argument("--loglevel", type=str,
-                            choices=['debug', 'info', 'warning', 'error',
-                                        'critical'],
-                            default='warning',
-                            help="Log level.")
-        parser.add_argument("--dump", type=str, default=None,
-                            help="File where to dump annotations for "
-                                    "comparisons.")
-
-        # parse command line arguments
-        args = parser.parse_args()
-
-        # initialize the Options class with command line arguments
-        Options(args)
 
     def setUp(self):
-        verbnet = verbnetreader.VerbnetReader(paths.Paths.verbnet_path("eng")).frames_for_verb
+        verbnet = verbnetreader.VerbnetReader(
+            paths.Paths.verbnet_path("eng")).frames_for_verb
         self.arg_guesser = ArgGuesser(verbnet)
 
     def test_global(self):
@@ -148,11 +76,12 @@ class ArgGuesserTest(unittest.TestCase):
             Arg(0, 20, "The others here today", "", True, "NP")
         ]
 
-        self.assertEqual(self.arg_guesser._find_args(treeBuilder.tree_list[0]), args)
+        self.assertEqual(self.arg_guesser._find_args(treeBuilder.tree_list[0]),
+                         args)
 
     def test_multiroot_sentence(self):
-        conll_tree = \
-"""1	because	because	IN	IN	-	15	VMOD	-	-
+        conll_tree = """\
+1	because	because	IN	IN	-	15	VMOD	-	-
 2	they	they	PRP	PRP	-	4	SUB	-	-
 3	just	just	RB	RB	-	4	VMOD	-	-
 4	say	say	VBP	VBP	-	1	SBAR	-	-
@@ -239,5 +168,86 @@ class ArgGuesserTest(unittest.TestCase):
         treeBuilder = SyntacticTreeBuilder(conll_tree)
         self.assertEqual(len(treeBuilder.tree_list), 6)
 
+
 if __name__ == '__main__':
-    unittest.main()
+    # parse command line arguments
+    parser = argparse.ArgumentParser(
+        description="""
+        Depending on the options used, can
+        # Annotate a single file
+        knowledgesrl.py --conll_input=parsed_file.conll
+                --conll_output=annotated_file.conll [options]
+
+        # Annotate FrameNet test set
+        knowledgesrl.py [options]
+
+        # Annotate FrameNet training set
+        knowledgesrl.py --training-set [options]
+
+        # Annotate FrameNet example corpus
+        knowledgesrl.py --lu [options]
+        """)
+    parser.add_argument("--language", "-l", type=str, choices=["eng", "fre"],
+                        default="eng",
+                        help="Name of the CoNLL-U file with the gold data.")
+    parser.add_argument("--best-gold", action="store_true",
+                        help=" Best configuration for gold.")
+    parser.add_argument("--best-auto", action="store_true",
+                        help="Best configuration for auto.")
+    parser.add_argument("--matching-algorithm", type=str,
+                        choices=["baseline", "sync_predicates",
+                                 "stop_on_fail"],
+                        default="sync_predicates",
+                        help="Select a frame matching algorithm.")
+    parser.add_argument("--add-non-core-args", action="store_true",
+                        help="Consider non-core-arg with gold arguments (why?)")
+    parser.add_argument("--model", type=str,
+                        choices=probabilitymodel.models,
+                        help="Probability models.")
+    parser.add_argument("--bootstrap", action="store_true",
+                        help="")
+    parser.add_argument("--no-argument-identification", action="store_true",
+                        help="Identify arguments automatically")
+    parser.add_argument("--heuristic-rules", action="store_true",
+                        help="Use Lang and Lapata heuristics to find args.")
+    parser.add_argument("--passivize", action="store_true",
+                        help="Handle passive sentences")
+    parser.add_argument("--semantic-restrictions", action="store_true",
+                        help="Restrict to phrases that obey VerbNet restrictions")
+    parser.add_argument("--wordnet-restrictions", action="store_true",
+                        help="Restrict to phrases that obey WordNet restrictions")
+    # what do we annotate?
+    parser.add_argument("--conll-input", "-i", type=str, default="",
+                        help="File to annotate.")
+    parser.add_argument("--conll-output", "-o", type=str, default=None,
+                        help="File to write result on. Default to stdout.")
+    parser.add_argument("--corpus", type=str,
+                        choices=["FrameNet", "dicoinfo_fr"],
+                        default=None,
+                        help="")
+    parser.add_argument("--training-set", action="store_true", default=True,
+                        help="To annotate FrameNet training set.")
+    parser.add_argument("--lu", action="store_true",
+                        help="To annotate FrameNet example corpus.")
+    # what kind of output do we want
+    parser.add_argument("--frame-lexicon", type=str,
+                        choices=["VerbNet", "FrameNet"],
+                        default="VerbNet",
+                        help="Chose frame lexicon to use for output.")
+    # meta
+    parser.add_argument("--loglevel", type=str,
+                        choices=['debug', 'info', 'warning', 'error',
+                                 'critical'],
+                        default='warning',
+                        help="Log level.")
+    parser.add_argument("--dump", type=str, default=None,
+                        help="File where to dump annotations for comparisons.")
+
+    # parse command line arguments
+    # args = parser.parse_args()
+    # Separate unittest arguments and custom arguments
+    args, unittest_args = parser.parse_known_args()
+
+    # initialize the Options class with command line arguments
+    Options(args)
+    unittest.main(argv=[sys.argv[0]] + unittest_args)
